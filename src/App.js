b0d8143802cf459976dd1c2472071be1ec800db0
contigen <69436315+contigen@users.noreply.github.com>
2023-03-01 17:04:18 +0100

    Start the audio recording feat

--- src/App.js
+++ src/App.js
@@ -2,16 +2,42 @@ import "./App.css";
 import { SpeechRecognition } from "./features/speech-recognition/";
 import { InternetAccessMessage } from "./features/ui";
 import { HomepageMessage } from "./features/ui/";
+import { useEffect } from "react";
 
 function App() {
+  async function getMedia(constraints) {
+    let stream = null;
+    try {
+      stream = await navigator.mediaDevices.getUserMedia(constraints);
+    } catch (err) {
+      console.log(err);
+    }
+    return stream;
+  }
+  navigator.mediaDevices
+    .getUserMedia({
+      audio: true,
+      video: { facingMode: `user` },
+    })
+    .then((mediaStream) => {
+      const audio = document.querySelector(`audio`);
+      audio.srcObject = mediaStream;
+      audio.onloadedmetadata = () => audio.play();
+    })
+    .catch((err) => console.log(err.name, err.message));
+  useEffect(() => {
+    getMedia({ audio: true });
+  });
+
   if (!(`webkitSpeechRecognition` in window || `SpeechRecognition` in window)) {
     return <HomepageMessage browserSupport={false} />;
   }
   return (
     <>
-      <HomepageMessage browserSupport/>
+      <HomepageMessage browserSupport />
       <SpeechRecognition />
       <InternetAccessMessage />
+      <audio id="video" controls></audio>
     </>
   );
 }
