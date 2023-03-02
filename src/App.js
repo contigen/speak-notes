13c3ebef08e055e2eb80dadf6363758ff86d72f3
contigen <69436315+contigen@users.noreply.github.com>
2023-03-02 12:39:19 +0100

    Add a test-feature

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { SpeechRecognition } from "./features/speech-recognition/";
 import { InternetAccessMessage } from "./features/ui";
 import { HomepageMessage } from "./features/ui/";
-import { useEffect, useRef,useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
   async function getMedia(constraints) {
@@ -32,24 +32,28 @@ function App() {
       })
       .catch((err) => console.log(err.name, err.message));
   }
-  const [val,setVal] = useState({current:null});
+  const [val, setVal] = useState({ current: `` });
   useEffect(() => {
     getMedia({ audio: true });
     navigator.mediaDevices
       .enumerateDevices()
       .then((devices) => {
         devices.forEach((device) => {
-          setVal((
-            {current:<span>
-              (`${device.kind}: ${device.label} id = ${device.deviceId}`)
-            </span>}
-          ))
+          console.log(
+            `${device.kind}: ${device.label} id = ${device.deviceId}`
+          );
+          setVal((prev) => ({
+            current:
+              prev.current +
+              ` ` +
+              `Device kind: ${device.kind}: Device label: ${device.label} id = ${device.deviceId}`,
+          }));
         });
       })
       .catch((err) => {
         console.error(`${err.name}: ${err.message}`);
       });
-  });
+  }, []);
 
   if (!(`webkitSpeechRecognition` in window || `SpeechRecognition` in window)) {
     return <HomepageMessage browserSupport={false} />;
@@ -61,7 +65,9 @@ function App() {
       <InternetAccessMessage />
       <audio id="video" controls></audio>
       <video id="video" controls></video>
-      <button>{val.current ?? `no value`}</button>
+      <p style={{ wordBreak: "break-word" }}>
+        {JSON.stringify(val.current) ?? `no value`}
+      </p>
     </>
   );
 }
