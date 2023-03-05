88756ffcc24e9d7130c5847f31626c9c9e0ad035
contigen <69436315+contigen@users.noreply.github.com>
2023-03-05 16:23:36 +0100

    Stop the audio recording feature

--- src/App.js
+++ src/App.js
@@ -2,36 +2,8 @@ import "./App.css";
 import { SpeechRecognition } from "./features/speech-recognition/";
 import { InternetAccessMessage } from "./features/ui";
 import { HomepageMessage } from "./features/ui/";
-import { useEffect, useCallback } from "react";
 
 function App() {
-  async function getMedia(constraints) {
-    let stream = null;
-    try {
-      stream = await navigator.mediaDevices.getUserMedia(constraints);
-    } catch (err) {
-      console.log(err);
-    }
-    return stream;
-  }
-  const mediaRecord = useCallback(
-    () =>
-      navigator.mediaDevices
-        .getUserMedia({
-          audio: true,
-        })
-        .then((mediaStream) => {
-          const audio = document.querySelector(`audio`);
-          audio.srcObject = mediaStream;
-          audio.onloadedmetadata = () => audio.play();
-        })
-        .catch((err) => console.log(err.name, err.message)),
-    []
-  );
-  useEffect(() => {
-    // getMedia({ audio: true });
-  }, []);
-
   if (!(`webkitSpeechRecognition` in window || `SpeechRecognition` in window)) {
     return <HomepageMessage browserSupport={false} />;
   }
@@ -40,8 +12,6 @@ function App() {
       <HomepageMessage browserSupport />
       <SpeechRecognition />
       <InternetAccessMessage />
-      <audio id="video" controls></audio>
-      <button onClick={mediaRecord}>Start media recording</button>
     </>
   );
 }
