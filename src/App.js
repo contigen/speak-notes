b641f093e473f977a182fd092b9ccf5979dd2555
contigen <69436315+contigen@users.noreply.github.com>
2023-04-05 22:16:58 +0100

    Add multiple notes feat

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 import { SpeechRecognition } from "./features/speech-recognition/";
 import { InternetAccessMessage } from "./features/ui";
 import { HomepageMessage } from "./features/ui/";
 
 function App() {
+  const [noteCount, setNoteCount] = useState([1]);
   if (!(`webkitSpeechRecognition` in window || `SpeechRecognition` in window)) {
     return <HomepageMessage browserSupport={false} />;
   }
+  const createSpeakNote = () => {
+    setNoteCount((prev) => [...prev, 1]);
+  };
+  const deleteSpeakNote = () => {
+    // const newArr = noteCount;
+    if (noteCount.length === 1) return;
+    setNoteCount((prev) => [...prev.slice(0, -1)]);
+  };
   return (
     <>
       <HomepageMessage browserSupport />
-      <SpeechRecognition />
+      <button onClick={createSpeakNote}>Add note</button>
+      <button onClick={deleteSpeakNote}>Delete note</button>
+      {noteCount.map((_, idx) => (
+        <SpeechRecognition key={idx} />
+      ))}
       <InternetAccessMessage />
       <pre>copy functionality</pre>
       <pre>add and delete notes</pre>
