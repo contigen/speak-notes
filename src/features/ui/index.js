7b03b20d58a1f3863aac8c6524e0b6433df4bd2b
contigen <69436315+contigen@users.noreply.github.com>
2023-04-18 21:24:17 +0100

    Addd InputFile component as an export

--- src/features/ui/index.js
+++ src/features/ui/index.js
@@ -2,3 +2,4 @@ export { Button } from "./button";
 export { InternetAccessMessage } from "./internet-access-message";
 export { TextArea } from "./textarea";
 export { HomepageMessage } from "./homepage-message";
+export { InputFile } from "./input";
