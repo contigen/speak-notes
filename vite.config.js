3507acdc6cbb4a3cadd0d6b30ee53963fac12291
contigen <69436315+contigen@users.noreply.github.com>
2023-07-08 17:48:11 +0100

    Modify vite-tsconfig-paths to ...js...

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import viteTsconfigPaths from "vite-tsconfig-paths";
+import viteJsconfigPaths from "vite-jsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
-});
+
+export default defineConfig(() => ({
+  plugins: [react(), viteJsconfigPaths(), svgrPlugin()],
+}));
